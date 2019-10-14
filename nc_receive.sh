4ebc7a2a2dfe84f5d7b327fff90c3710a7a88922
Diphia <zqygodness@gmail.com>
2019-10-14 15:33:50 +0800

    nc_receive.sh added

---
+++ nc_receive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# nc_receive.sh
+# diphia@2019
+# This script is used to receive a file from target machine via nc 
+
+ip=10.73.151.20
+port=4444
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: nc_receive [renamed file name]"
+	echo "Example: nc_receive img001.jpg"
+	exit
+fi
+
+nc -nv ${ip} ${port} > $1
