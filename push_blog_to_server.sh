54f8f1e20dfc36901ea0256d5a35ca046d60b510
Diphia <zqygodness@gmail.com>
2019-11-01 19:22:36 +0800

    rsync update

--- push_blog_to_server.sh
+++ push_blog_to_server.sh
@@ -3,7 +3,7 @@
 # diphia@2019
 # This script is used to push the local static web pages to the server, the IP address and file location should be set before. After pushing, script will create a log automatically.
 
-remote_username="username"
+remote_username="ubuntu"
 remote_ip="49.234.63.85"
 remote_location="~/"
 hugo_location="/home/diphia/Westfall"
@@ -17,6 +17,7 @@ fi
 
 echo " " >> ${hugo_location}/push_history.log
 echo " " >> ${hugo_location}/push_history.log
+echo "------------------------------------" >> ${hugo_location}/push_history.log
 echo "Date:" >> ${hugo_location}/push_history.log
 
 date >> ${hugo_location}/push_history.log
@@ -29,6 +30,6 @@ echo " " >> ${hugo_location}/push_history.log
 
 echo "Rsync:" >> ${hugo_location}/push_history.log
 rsync -avz -e'ssh -p 22' ${hugo_location}/public ${remote_username}@${remote_ip}:${remote_location} | tail -n 2 >> ${hugo_location}/push_history.log
+echo "------------------------------------" >> ${hugo_location}/push_history.log
 
 #echo ${hugo_location}/public ${remote_username}@${remote_ip}:${remote_location}
-
