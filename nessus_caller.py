86d2f6ea3327d9da273daefe5cdec46c42bb21c3
Diphia <zqygodness@gmail.com>
2019-11-07 10:08:48 +0800

    nessus_caller.py added

---
+++ nessus_caller.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+# nessus_caller.py
+# diphia@2019
+# This script is used to send some commands to nessus and fetch the result
+
+import requests
+import json
+#import certifi
+#import urllib3
+
+nessus_ip='10.73.151.65'
+nessus_port='8834'
+nessus_username='root'
+nessus_password='cmcc#123'
+
+access_key='a655bd9c0d16f00656efbfcd5ad0cd31fefc64b12d2d6778d341277f6988dc5c'
+secret_key='0724b4ef60d80bf1c3cee8eac4d3aa3155342846dfcec5009e05e0632346b59b'
+
+
+url='https://'+nessus_ip+':'+nessus_port
+
+'''
+def login():
+    token=''
+    url_session=url+'/session'
+    id_pass={'username':nessus_username,'password':nessus_password}
+    response=requests.post(url_session,data=id_pass,verify=False)
+    if response.status_code == 200:
+        token=json.loads(response.text)['token']
+    print('token:'+token)
+    return token
+'''
+
+
+def get_scan_list():
+    result=''
+    url_scans=url+'/scans'
+    global header
+    header={'X-ApiKeys':'accessKey={access_key};secretKey={secret_key}'.format(access_key=access_key,secret_key=secret_key),
+            'Content-type':'application/json',
+            'Accept':'text/plain'} 
+    response=requests.get(url_scans,headers=header,verify=False)
+    if response.status_code == 200:
+        result=json.loads(response.text)
+    print(response)
+    return result
+
+'''
+def get_scan_list():
+    result=''
+    token=login()
+    global header_x
+    if token != '':
+        url_scans=url+'/scans'
+        header_x={'X-Cookie':'token={token};'.format(token=token),
+                'Content-type':'application/json',
+                'Accept':'text/plain'}
+        response=requests.get(url_scans,headers=header_x,verify=False)
+        if response.status_code == 200:
+            result=json.loads(response.text)
+    return result
+'''
+
+def get_scan_id(scanname):
+    scan_id=0
+    scan_list=get_scan_list()['scans']
+    if scan_list != '':
+        for scan in scan_list:
+            if scan['name'] == scanname:
+                scan_id=scan['id']
+                break
+    return scan_id
+
+def scan_launch(iplist):
+    url_launch=url+'/scans/{scan_id}/launch'.format(scan_id=get_scan_id('test1'))
+    #url_launch=url+'/scans/{scan_id}/launch'.format(scan_id=5)
+    print(url_launch)
+    data={
+            'alt_targets':iplist
+    }
+    response=requests.post(url_launch,headers=header,data=data,verify=False)
+    if response.status_code == 200:
+        return True
+    else:
+        print(response)
+        return False
+
+
+#print(get_scan_list()['scans'])
+#print(get_scan_list())
+#print(get_scan_id('test1'))
+print(scan_launch(['10.73.151.59']))
+
