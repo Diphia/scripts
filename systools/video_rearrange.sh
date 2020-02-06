dc75ad01534a00ae18e1f5d7a61207b84d5df3e1
Diphia <zqygodness@gmail.com>
2020-02-06 17:16:00 +0800

    update

--- systools/video_rearrange.sh
+++ systools/video_rearrange.sh
@@ -7,19 +7,21 @@ PROCESS_TARGET="/mnt/media/transmission_move_target"
 THRESHOLD="100" # unit is M
 
 valid_list=`find ${PROCESS_TARGET} -size +${THRESHOLD}M`
-
 for valid_items in ${valid_list}:
 do
     echo "now processing:${valid_items}"
     mv ${valid_items} ${PROCESS_TARGET}
 done
 
-directory_list=`ls -al ${PROCESS_TARGET} | awk '(substr($1,1,1)=="d")&&($9!=".")&&($9!="..")&&($9!="."){print $9}'`
+directory_list=`ls -al ${PROCESS_TARGET} | awk '(substr($1,1,1)=="d")&&($9!=".")&&($9!=".."){print $9}'`
 for directory in ${directory_list}
 do
-    if [[ `du -m ${directory}|awk '{print $1}'` -lt 100 ]]
+    if [[ `du -m ${directory}|awk '{print $1}'` -lt ${THRESHOLD} ]]
     then
-        rm -r ${directory}
+        rm -ri ${directory}
         echo "${directory} removed"
+    else
+        echo "${directory} is larger than ${THRESHOLD}M, you need to check it."
     fi
 done
+
