a754a987af159620389446b2e9ef963ff81ceac9
Diphia <zqygodness@gmail.com>
2020-02-12 19:09:20 +0800

    update

---
+++ burp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# burp.sh
+# diphia@2019
+# This script is used to run the burp suite pro (cracked version) in a simple way
+
+DIR=/home/diphia/burpsuite_pro
+
+java -jar ${DIR}/BurpHelper.jar
