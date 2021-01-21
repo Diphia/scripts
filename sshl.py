06316c7270067b8bcb9b5177b5a5b1e4f2895547
diphia <zqygodness@gmail.com>
2021-01-21 11:31:00 +0800

    when the host is too long, use .. and still fix the length

--- sshl.py
+++ sshl.py
@@ -3,6 +3,8 @@
 # diphia@2021
 # This script is used to show the saved ssh targets
 import platform
+from termcolor import colored
+
 
 user = 'diphia'
 
@@ -15,11 +17,16 @@ def read_server():
     current_server = {}
     with open(ssh_config,'r') as f:
         for line in f:
+            if(line == '\n'):
+                continue
             if(line.split(' ')[0] == 'Host'):
                 server_list.append(current_server)
                 current_server = {}
             splited_line = line.strip().split(' ')
             key, value = splited_line[0], splited_line[1]
+            if(key == 'Host'):
+                if(len(value)>10):
+                    value = value[0:7]+'..'
             current_server[key]=value
     server_list.append(current_server)
     del server_list[0]
@@ -27,7 +34,7 @@ def read_server():
 
 def print_server(server_list):
     for server in server_list:
-        print(server['Host'].ljust(10)+': '+server['User']+'@'+server['HostName']+':'+server['Port'])
+        print(server['Host'].ljust(10)+': '+ server['User']+'@'+server['HostName']+':'+server['Port'])
 
 if __name__=="__main__":
     server_list = read_server()
