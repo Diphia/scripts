b43e4433b7dbd6b54b181ed63a1328bd8f69ef8d
diphia <zqygodness@gmail.com>
2021-01-26 17:46:24 +0800

    update

--- arch_vm_bootstrap.sh
+++ arch_vm_bootstrap.sh
@@ -6,4 +6,4 @@ xrandr --dpi 150
 sudo ip link set enp0s3 up && sudo dhcpcd enp0s3
 sudo ip link set enp0s8 up && sudo dhcpcd enp0s8
 
-VBoxClient-all && sudo mount -t vboxsf Downloads /mnt/shared_download && sudo mount -t vboxsf iCloudDrive /mnt/icloud
+VBoxClient-all && sudo mount -t vboxsf iCloudDrive /mnt/icloud && cd /mnt/shared_download && sudo mount -t vboxsf Downloads /mnt/shared_download
